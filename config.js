c2a53ad5d77557a137df390255ab75ebe8de6ddb
ハミド <90818589+Hamid021206@users.noreply.github.com>
2026-04-29 21:02:43 +0700

    Create config.js with bot configuration for Asano

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    botName: 'Asano',
+    owner: 'Zen',
+    prefix: '.',
+    ownerNumbers: ['923357204149', '6282371973833'],
+    botNumber: '6285185484189',
+    groupLink: '', // Add the group link here
+    languages: ['Indonesian', 'Japanese'],
+    features: 'all'
+};
